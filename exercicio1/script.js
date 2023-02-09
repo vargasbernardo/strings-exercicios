d4124b6b30d69c0d83e170a51919e1b77cbe98b4
vargasbernardo <vargasbernardo97@gmail.com>
2023-02-09 00:07:37 -0300

    novo teste

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,26 +1,4 @@
-// # Exercício 1
-
-// Faça um programa que pergunte ao usuário seu **nome** e suas **três comidas favoritas**. Em seguida, imprima no console a seguinte mensagem:
-    
-   
-//     Estas são as comidas favoritas de nomeDoUsuario:
-//     - Comida1
-//     - Comida2
-//     - Comida3
-  
-    
-// Você deve usar apenas um `console.log()` para isso
-
-//   <details> 
-//   <summary> 💡 Dica</summary>
-  
-//     ⭐ Você pode juntar texto com variáveis de duas formas:
-//     - Concatenando as strings (com o sinal de +)
-//     - Utilizando template strings (e envolvendo a sua string com o sinal de CRASE ao invés de aspas ⇒ ````)
-    
-//     Adicionar um  `\n` na string coloca uma **quebra de linha** no texto.
-  
-//   </details>
+// Exercicio 1 feito
 const nomeUsuario = prompt('Digite seu nome: ')
 const comidaFavorita1 = prompt('Sua comida favorita: ')
 const comidaFavorita2 = prompt('Sua segunda comida favorita: ')
